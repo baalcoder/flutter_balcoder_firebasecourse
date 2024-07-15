374d88b4f8003542538df3c7e8d8516e2c43ba2c
Balcoder <64577287+baalcoder@users.noreply.github.com>
2024-07-15 16:39:25 -0500

    Update to resolve alerts Google API Key main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_balcoder_firebasecourse/ui/home_page.dart';
 
 main() async {
-  await Firebase.initializeApp(
-      options: FirebaseOptions(
-          apiKey: "AIzaSyAnkeGuTDM347o8cFZ3-tAb_EAu-PeT5aA",
-          authDomain: "cursofirebase-balcoder.firebaseapp.com",
-          projectId: "cursofirebase-balcoder",
-          storageBucket: "cursofirebase-balcoder.appspot.com",
-          messagingSenderId: "98866720545",
-          appId: "1:98866720545:web:cfba0f54c5f94df9379860",
-          measurementId: "G-XNM65XQ796"));
-
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
